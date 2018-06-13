caaa3df81a89136347c1251431df47f40f5463de
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:44:14 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Action/PostActionHttpTrigger/PostActionHttpTrigger.cs
+++ NCS.DSS.Action/PostActionHttpTrigger/PostActionHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Action.PostActionHttpTrigger
     public static class PostActionHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Actions/")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Action))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Actions/")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Action C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var action = await req.Content.ReadAsAsync<Models.Action>();
+
+            var actionService = new PostActionHttpTriggerService();
+            var actionId = actionService.Create(action);
+
+            return actionId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Action record with Id of : " + actionId)
+                };
         }
     }
 }
--- NCS.DSS.Action/PostActionHttpTrigger/PostActionHttpTriggerService.cs
+++ NCS.DSS.Action/PostActionHttpTrigger/PostActionHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Action.PostActionHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Action.PostActionHttpTrigger
 {
     public class PostActionHttpTriggerService
     {
+        public Guid? Create(Models.Action action)
+        {
+            if (action == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
