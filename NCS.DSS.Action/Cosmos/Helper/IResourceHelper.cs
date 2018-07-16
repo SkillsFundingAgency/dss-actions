b55d6d5736f1e4f967f5de219e02b04bb6c20901
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-16 14:27:42 +0100

    Add Cosmos

---
+++ NCS.DSS.Action/Cosmos/Helper/IResourceHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NCS.DSS.Action.Cosmos.Helper
+{
+    public interface IResourceHelper
+    {
+        bool DoesCustomerExist(Guid customerId);
+        bool DoesInteractionExist(Guid interactionId);
+    }
+}
