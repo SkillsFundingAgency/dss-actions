53fbe32590a7dfc1e10ea942816991419b171bd1
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-16 14:28:48 +0100

    Add Http Response/Request Messages Helpers

---
+++ NCS.DSS.Action/Helpers/HttpRequestMessageHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.Action.Helpers
+{
+    public class HttpRequestMessageHelper : IHttpRequestMessageHelper
+    {
+        public async Task<T> GetActionPlanFromRequest<T>(HttpRequestMessage req)
+        {
+            return await req.Content.ReadAsAsync<T>();
+        }
+    }
+}
---
+++ NCS.DSS.Action/Helpers/IHttpRequestMessageHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.Action.Helpers
+{
+    public interface IHttpRequestMessageHelper
+    {
+        Task<T> GetActionPlanFromRequest<T>(HttpRequestMessage req);
+    }
+}
