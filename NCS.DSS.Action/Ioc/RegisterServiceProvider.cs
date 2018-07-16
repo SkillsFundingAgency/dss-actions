8042bbbad7c2d5dc07ee680bcb484d7c4454f273
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-16 14:27:57 +0100

    Add IOC

---
+++ NCS.DSS.Action/Ioc/RegisterServiceProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NCS.DSS.Action.Cosmos.Helper;
+using NCS.DSS.Action.GetActionByIdHttpTrigger.Service;
+using NCS.DSS.Action.GetActionHttpTrigger.Service;
+using NCS.DSS.Action.Helpers;
+using NCS.DSS.Action.PatchActionHttpTrigger.Service;
+using NCS.DSS.Action.PostActionHttpTrigger.Service;
+using NCS.DSS.Action.Validation;
+
+
+namespace NCS.DSS.Action.Ioc
+{
+    public class RegisterServiceProvider
+    {
+        public IServiceProvider CreateServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IGetActionHttpTriggerService, GetActionHttpTriggerService>();
+            services.AddTransient<IGetActionByIdHttpTriggerService, GetActionByIdHttpTriggerService>();
+            services.AddTransient<IPostActionHttpTriggerService, PostActionHttpTriggerService>();
+            services.AddTransient<IPatchActionHttpTriggerService, PatchActionHttpTriggerService>();
+            services.AddTransient<IResourceHelper, ResourceHelper>();
+            services.AddTransient<IValidate, Validate>();
+            services.AddTransient<IHttpRequestMessageHelper, HttpRequestMessageHelper>();
+            return services.BuildServiceProvider(true);
+        }
+    }
+}
