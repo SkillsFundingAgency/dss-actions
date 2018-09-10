ad827dc115e3b1226b71a61afadb835adc604016
Anil Dhiman <anil.dhiman@outlook.com>
2018-09-10 15:50:22 +0100

    remove extra slash

--- NCS.DSS.Action/ServiceBus/ServiceBusClient.cs
+++ NCS.DSS.Action/ServiceBus/ServiceBusClient.cs
@@ -27,7 +27,7 @@ namespace NCS.DSS.Action.ServiceBus
                 TitleMessage = "New Action record {" + action.ActionPlanId + "} added at " + DateTime.UtcNow,
                 CustomerGuid = action.CustomerId,
                 LastModifiedDate = action.LastModifiedDate,
-                URL = reqUrl + "/" + action.ActionPlanId,
+                URL = reqUrl + action.ActionPlanId,
                 IsNewCustomer = false,
                 TouchpointId = action.LastModifiedTouchpointId
             };
