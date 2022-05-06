7d11ad9c4c4be4270717757a3111e21e384f45fe
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2022-05-06 08:57:56 +0100

    add validation to make subcontractorId mandatory

--- NCS.DSS.Action/GetActionHttpTrigger/Function/GetActionHttpTrigger.cs
+++ NCS.DSS.Action/GetActionHttpTrigger/Function/GetActionHttpTrigger.cs
@@ -67,6 +67,13 @@ namespace NCS.DSS.Action.GetActionHttpTrigger.Function
                 return _httpResponseMessageHelper.BadRequest();
             }
 
+            var subcontractorId = _httpRequestHelper.GetDssSubcontractorId(req);
+            if (string.IsNullOrEmpty(subcontractorId))
+            {
+                _loggerHelper.LogInformationMessage(log, correlationGuid, "Unable to locate 'SubcontractorId' in request header");
+                return _httpResponseMessageHelper.BadRequest();
+            }
+
             _loggerHelper.LogInformationMessage(log, correlationGuid,
                 string.Format("Get Action C# HTTP trigger function  processed a request. By Touchpoint: {0}",
                     touchpointId));
--- NCS.DSS.Action/PatchActionHttpTrigger/Function/PatchActionHttpTrigger.cs
+++ NCS.DSS.Action/PatchActionHttpTrigger/Function/PatchActionHttpTrigger.cs
@@ -78,6 +78,13 @@ namespace NCS.DSS.Action.PatchActionHttpTrigger.Function
                 return _httpResponseMessageHelper.BadRequest();
             }
 
+            var subcontractorId = _httpRequestHelper.GetDssSubcontractorId(req);
+            if (string.IsNullOrEmpty(subcontractorId))
+            {
+                _loggerHelper.LogInformationMessage(log, correlationGuid, "Unable to locate 'SubcontractorId' in request header");
+                return _httpResponseMessageHelper.BadRequest();
+            }
+
             var apimUrl = _httpRequestHelper.GetDssApimUrl(req);
             if (string.IsNullOrEmpty(apimUrl))
             {
