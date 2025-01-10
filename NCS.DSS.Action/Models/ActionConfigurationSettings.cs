d85b779c09c35b19720f4f87a2ae347144b76767
hnandra <h_nandra@hotmail.co.uk>
2025-01-10 17:55:43 +0000

    Add configuration settings class

---
+++ NCS.DSS.Action/Models/ActionConfigurationSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace NCS.DSS.Action.Models;
+
+public class ActionConfigurationSettings
+{
+    public required string CosmosDBConnectionString { get; set; }
+    public required string ServiceBusConnectionString { get; set; }
+    public required string QueueName { get; set; }
+    public required string DatabaseId { get; set; }
+    public required string CollectionId { get; set; }
+    public required string ActionPlanDatabaseId { get; set; }
+    public required string ActionPlanCollectionId { get; set; }
+    public required string CustomerDatabaseId { get; set; }
+    public required string CustomerCollectionId { get; set; }
+    public required string InteractionDatabaseId { get; set; }
+    public required string InteractionCollectionId { get; set; }
+}
